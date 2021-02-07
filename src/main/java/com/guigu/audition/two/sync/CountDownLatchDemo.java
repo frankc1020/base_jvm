b8467b614e782ee7905c9e6e7b52976fcea33713
guzc <gucong19961020@163.com>
2021-02-07 21:12:47 +0800

    线程CountDownLatch用法

--- src/main/java/com/guigu/audition/two/sync/CountDownLatchDemo.java
+++ src/main/java/com/guigu/audition/two/sync/CountDownLatchDemo.java
@@ -3,6 +3,10 @@ import java.util.concurrent.CountDownLatch;
 /**
  * CountDownLatch，是控制某个线程在最后执行，只有当前面的线程
  * 执行完毕，才可以往下进行执行
+ *
+ * countDownLatch 是多个线程允许阻塞在一个地方，直至所有的线程的任务都执行完毕。
+ *
+ *
  */
 public class CountDownLatchDemo {
     public static void main(String[] args) throws InterruptedException {
@@ -11,6 +15,7 @@ public class CountDownLatchDemo {
             new Thread(() -> {
                 System.out.println(Thread.currentThread().getName() + "\t 国，被灭");
                 countDownLatch.countDown();
+                System.out.println("jggjh");
             },CountryEnum.forEach_CountryEnum(i).getRetMessage()).start();
         }
         countDownLatch.await();//只用当countDownLatch减到0的时候才可以
