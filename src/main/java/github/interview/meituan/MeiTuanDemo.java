49467699c10d5fe4aa206fd849e21be8e833e4ab
guzc <gucong19961020@163.com>
2021-03-07 18:53:52 +0800

    练习

---
+++ src/main/java/github/interview/meituan/MeiTuanDemo.java
@@ -0,0 +1,16 @@
+package github.interview.meituan;
+
+/**
+ * @author admin
+ * @title: MeiTuanDemo
+ * @projectName base_thread
+ * @description: TODO
+ * @date 2021/1/8 08:55
+ */
+public class MeiTuanDemo {
+
+    public static void main(String[] args) {
+        System.out.println(3|9);
+    }
+
+}
